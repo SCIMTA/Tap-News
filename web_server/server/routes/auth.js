56eedbd40c913c2e72af04e72b1e4c241e522e6d
Tung Nguyen <tungnc3142@gmail.com>
2021-11-20 00:09:00 +0700

    remove 8 lenth password

--- web_server/server/routes/auth.js
+++ web_server/server/routes/auth.js
@@ -1,78 +1,78 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
-const validator = require('validator');
+const validator = require("validator");
 
-
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const validationResult = validateSignupForm(req.body);
   if (!validationResult.success) {
-    console.log('validationResult failed');
+    console.log("validationResult failed");
     return res.status(400).json({
       success: false,
       message: validationResult.message,
-      errors: validationResult.errors
+      errors: validationResult.errors,
     });
   }
 
-  return passport.authenticate('local-signup', err => {
+  return passport.authenticate("local-signup", (err) => {
     if (err) {
       console.log(err);
-      if (err.name === 'MongoError' && err.code === 11000) {
+      if (err.name === "MongoError" && err.code === 11000) {
         // the 11000 Mongo code is for a duplication email error
         // the 409 HTTP status code is for conflict error
         return res.status(409).json({
           success: false,
-          message: 'Check the form for errors.',
+          message: "Check the form for errors.",
           errors: {
-            email: 'This email is already taken.'
-          }
+            email: "This email is already taken.",
+          },
         });
       }
 
       return res.status(400).json({
         success: false,
-        message: 'Could not process the form.'
+        message: "Could not process the form.",
       });
     }
 
     return res.status(200).json({
       success: true,
-      message: 'You have successfully signed up! Now you should be able to log in.'
+      message:
+        "You have successfully signed up! Now you should be able to log in.",
     });
   })(req, res, next);
 });
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const validationResult = validateLoginForm(req.body);
   if (!validationResult.success) {
     return res.status(400).json({
       success: false,
       message: validationResult.message,
-      errors: validationResult.errors
+      errors: validationResult.errors,
     });
   }
 
-  return passport.authenticate('local-login', (err, token, userData) => {
+  return passport.authenticate("local-login", (err, token, userData) => {
     if (err) {
-      if (err.name === 'IncorrectCredentialsError') {
+      if (err.name === "IncorrectCredentialsError") {
         return res.status(400).json({
           success: false,
-          message: err.message
+          message: err.message,
         });
       }
 
       return res.status(400).json({
         success: false,
-        message: 'Could not process the form: ' + err.message
+        message: "Could not process the form: " + err.message,
       });
     }
 
     return res.json({
       success: true,
-      message: 'You have successfully logged in!',
+      message: "You have successfully logged in!",
       token,
-      user: userData
+      user: userData,
     });
   })(req, res, next);
 });
@@ -81,26 +81,34 @@ function validateSignupForm(payload) {
   console.log(payload);
   const errors = {};
   let isFormValid = true;
-  let message = '';
+  let message = "";
 
-  if (!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)) {
+  if (
+    !payload ||
+    typeof payload.email !== "string" ||
+    !validator.isEmail(payload.email)
+  ) {
     isFormValid = false;
-    errors.email = 'Please provide a correct email address.';
+    errors.email = "Please provide a correct email address.";
   }
 
-  if (!payload || typeof payload.password !== 'string' || payload.password.length < 8) {
+  if (
+    !payload ||
+    typeof payload.password !== "string" ||
+    payload.password.length < 1
+  ) {
     isFormValid = false;
-    errors.password = 'Password must have at least 8 characters.';
+    errors.password = "Password must have at least 1 characters.";
   }
 
   if (!isFormValid) {
-    message = 'Check the form for errors.';
+    message = "Check the form for errors.";
   }
 
   return {
     success: isFormValid,
     message,
-    errors
+    errors,
   };
 }
 
@@ -108,26 +116,34 @@ function validateLoginForm(payload) {
   console.log(payload);
   const errors = {};
   let isFormValid = true;
-  let message = '';
+  let message = "";
 
-  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0) {
+  if (
+    !payload ||
+    typeof payload.email !== "string" ||
+    payload.email.trim().length === 0
+  ) {
     isFormValid = false;
-    errors.email = 'Please provide your email address.';
+    errors.email = "Please provide your email address.";
   }
 
-  if (!payload || typeof payload.password !== 'string' || payload.password.length === 0) {
+  if (
+    !payload ||
+    typeof payload.password !== "string" ||
+    payload.password.length === 0
+  ) {
     isFormValid = false;
-    errors.password = 'Please provide your password.';
+    errors.password = "Please provide your password.";
   }
 
   if (!isFormValid) {
-    message = 'Check the form for errors.';
+    message = "Check the form for errors.";
   }
 
   return {
     success: isFormValid,
     message,
-    errors
+    errors,
   };
 }
 
