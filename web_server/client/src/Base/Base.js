cdebb27a246e5af81784dd5f475c4e293ea1f6fd
Tung Nguyen <tungnc3142@gmail.com>
2021-11-19 21:25:37 +0700

    translate web

--- web_server/client/src/Base/Base.js
+++ web_server/client/src/Base/Base.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router';
-import Auth from '../Auth/Auth';
+import React from "react";
+import { Link } from "react-router";
+import Auth from "../Auth/Auth";
 
 class Base extends React.Component {
-    render(){
-        return(
-            <div>
-              <nav className='nav-bar indigo lighten-1'>
-                <div className='nav-wrapper'>
-                  <a href='/' className='brand-logo'>&nbsp;&nbsp;Tap News</a>
-                  <ul className='right' id='nav-mobile'>
-                    {Auth.isUserAuthenticated() ?
-                      (<div>
-                          <li>{Auth.getEmail()}</li>
-                          <li><Link to='/logout'>Log out</Link></li>
-                       </div>)
-                       :
-                       (<div>
-                           <li><Link to='/login'>Log in</Link></li>
-                           <li><Link to='/signup'>Sign up</Link></li>
-                        </div>)
-                    }
-                  </ul>
+  render() {
+    return (
+      <div>
+        <nav className="nav-bar indigo lighten-1">
+          <div className="nav-wrapper">
+            <a href="/" className="brand-logo">
+              &nbsp;&nbsp;Tin tức Bất động sản
+            </a>
+            <ul className="right" id="nav-mobile">
+              {Auth.isUserAuthenticated() ? (
+                <div>
+                  <li>{Auth.getEmail()}</li>
+                  <li>
+                    <Link to="/logout">Đăng xuất</Link>
+                  </li>
                 </div>
-              </nav>
-              <br/>
-              {this.props.children}
-            </div>
-        )
-    }
-
+              ) : (
+                <div>
+                  <li>
+                    <Link to="/login">Đăng nhập</Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">Đăng ký</Link>
+                  </li>
+                </div>
+              )}
+            </ul>
+          </div>
+        </nav>
+        <br />
+        {this.props.children}
+      </div>
+    );
+  }
 }
-
 
 export default Base;
